bb24e8d964f2a07f6cf6fb1e692ec2586dec4e80
Kaveesha Silva <47806601+KaveeshaSilva@users.noreply.github.com>
2022-09-22 12:47:59 +0530

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <Header/>
+    <OrgChart/>
+    <!-- <test/> -->
+
+   
+  </div>
+</template>
+
+<script>
+
+ 
+  import OrgChart from './composant/OrgChart.vue'
+
+  export default {
+
+    components: { 
+      OrgChart,
+    },
+
+    name: 'app',
+    data () {
+      return {
+      }
+    }
+  }
+</script>
+
+<style lang="scss" src="./style/app.css">
+</style>
+
+
+
+
+
---
+++ src/composant/OrgChart.vue
@@ -0,0 +1,77 @@
+<template>
+  <vo-basic
+    :data="chartData"
+    :draggable="true"
+    :pan="true"
+    :zoom="true"
+    :exportButton="true"
+  ></vo-basic>
+</template>
+
+<script>
+import { VoBasic } from "vue-orgchart";
+
+export default {
+  components: { VoBasic },
+
+  created() {
+    this.chartData = {
+      name: "Parent",
+      title: "Machin",
+
+      children: [
+        {
+          name: "Child-1",
+          children: [
+            { name: "Last_Child-1", children: [{ name: "1" }, { name: "1" }] },
+            { name: "Last_Child-2", children: [{ name: "1" }, { name: "1" }] },
+          ],
+        },
+
+        {
+          name: "Child-2",
+          children: [
+            { name: "Last_Child-1", children: [{ name: "1" }, { name: "1" }] },
+            { name: "Last_Child-2", children: [{ name: "1" }, { name: "1" }] },
+          ],
+        },
+
+        {
+          name: "Child-3",
+          children: [
+            { name: "Last_Child-1", children: [{ name: "1" }, { name: "1" }] },
+            { name: "Last_Child-2", children: [{ name: "1" }, { name: "1" }] },
+          ],
+        },
+
+        /* { 
+                    name: 'Child-4' ,
+                    children: [
+                        { name: 'Last_Child-1', children: [ {name:'1'}, {name:'1'} ]},
+                        { name: 'Last_Child-2', children: [ {name:'1'}, {name:'1'} ] }
+                    ]
+                },    
+                
+                { 
+                    name: 'Child-5',
+                    children: [
+                        { name: 'Last_Child-1', children: [ {name:'1'}, {name:'1'} ]},
+                        { name: 'Last_Child-2', children: [ {name:'1'}, {name:'1'} ]}
+                    ]
+                },
+
+                {
+                    name: 'Child-6',
+                    children: [
+                        { name: 'Last_Child-1', children: [ {name:'1'}, {name:'1'} ] },
+                        { name: 'Last_Child-2', children: [ {name:'1'}, {name:'1'} ] }
+                    ]
+                } */
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="scss" src="../style/orgChart.css">
+</style>
